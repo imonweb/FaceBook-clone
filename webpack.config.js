2ad4c0af3a10e6a1292584b8eb4d6e9e47b82a5f
imonweb <contact@imonweb.co.uk>
2021-07-15 23:54:32 +0100

    initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,22 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: 'myBundle.js',
   },
-  
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: 
+      }
+    ]
+  }
+  devServer: {
+    before: function(src, server){
+      server.__watch('./src/*.html')
+    },
+    contentBase: path.join(__dirname, 'src'),
+    hot: true,
+    port: 3000,
+  },
+
+
 }
